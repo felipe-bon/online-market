d9af9234f7a941863ec16f47cfb2803b64099ded
felipe-bon <felipebonetti2001@hotmail.com>
2024-07-09 17:21:46 -0300

    added department and products routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,22 @@
 import { Router, Request, Response } from "express";
 import { UserController } from "./controllers/UserControllers";
 import { authMiddleware } from "./middlewares/authMiddleware";
+import { ProductController } from "./controllers/ProductControllers";
+import { DepartmentController } from "./controllers/DepartmentsController";
 const routes = Router()
+
+routes.get('/products/:id', new ProductController().getProduct)
+routes.post('/products', new ProductController().create)
+routes.put('/products/:id', new ProductController().updateProduct)
+routes.get('/products', new ProductController().getAll)
+routes.delete('/products/:id', new ProductController().deleteProduct)
+
+routes.post('/departments', new DepartmentController().create)
+routes.get('/departments', new DepartmentController().getAll)
+routes.get('/departments/:id', new DepartmentController().getDepartment)
+routes.get('/departments/:departmentName/products', new ProductController().getProductsByDepartment)
+routes.put('/departments/:id', new DepartmentController().updateDepartment)
+routes.delete('/departments/:id', new DepartmentController().deleteDepartment)
 
 routes.post('/user', new UserController().create)
 routes.post('/login', new UserController().login)
