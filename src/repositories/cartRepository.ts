7ea89518db5a54478ceb7dece9cdb53a9618ba4c
felipe-bon <felipebonetti2001@hotmail.com>
2024-07-09 21:08:55 -0300

    fixed Cart repository

--- src/repositories/cartRepository.ts
+++ src/repositories/cartRepository.ts
@@ -1,4 +1,4 @@
 import { AppDatasource } from "../data-source";
 import { Cart } from "../entities/Cart";
 
-export const DepartmentRepository = AppDatasource.getRepository(Cart)
+export const cartRepository = AppDatasource.getRepository(Cart)
